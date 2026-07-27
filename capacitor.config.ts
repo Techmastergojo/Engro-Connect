6e014df6bbf234aa8e6aa9d2aaffe9742e0c11b6
techmastergojo <hmzetehsin@gmail.com>
2026-07-27 13:35:20 +0500

    feat: rename app to Deodar-SKT, package id com.coordinate.helper.deodarskt

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.coordinate.helper',
-  appName: 'Coordinate Helper',
+  appId: 'com.coordinate.helper.deodarskt',
+  appName: 'Deodar-SKT',
   webDir: 'dist'
 };
 
