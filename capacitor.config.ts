5cd1f464d5546cef67a1dd9dca1ec2e190ffacf6
techmastergojo <hmzetehsin@gmail.com>
2026-07-27 13:47:04 +0500

    feat: load Deodar GRW1 data - 150 sites, rename app to Deodar-GRW1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.coordinate.helper',
-  appName: 'Deodar-SKT',
+  appName: 'Deodar-GRW1',
   webDir: 'dist'
 };
 
